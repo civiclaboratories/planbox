4a4f5b0af511a7fa153f6a906c365d490af00746
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2015-05-05 08:25:11 -0700

    Throttle rendering of the Shareabouts dashboard data

--- src/planbox_ui/static/scripts/project-dashboard/shareabouts-dataset-unique-contrib-count-widget-view.js
+++ src/planbox_ui/static/scripts/project-dashboard/shareabouts-dataset-unique-contrib-count-widget-view.js
@@ -5,6 +5,28 @@ var Planbox = Planbox || {};
 (function(NS, $) {
   'use strict';
 
+  _.nowAndLater = function(func, wait) {
+    // Run the wrapped function at least twice: once at the beginning
+    // of a debounced period, and once after the last debounced call.
+
+    var context, funcArgs, result, later;
+    var debouncedFunc = _.debounce(func, wait, true);
+    var now = function() {
+      if (later) { clearTimeout(later); later = null; }
+      debouncedFunc.apply(context, funcArgs);
+    };
+
+    return function() {
+      context = this;
+      funcArgs = arguments;
+
+      result = now();                    // Now ...
+      later = setTimeout(now, wait+50);  // ... and later.
+
+      return result;
+    };
+  };
+
   NS.DatasetUniqueContribCountWidgetView = Backbone.Marionette.ItemView.extend({
     template: '#dataset-unique-contrib-count-widget-tpl',
     modelEvents: {
@@ -12,6 +34,8 @@ var Planbox = Planbox || {};
     },
     initialize: function(options) {
       this.plugin = options.plugin;
+
+      this.render = _.nowAndLater(this.render, 200);
 
       this.plugin.places.on('add', _.bind(this.render, this));
       this.plugin.submissions.on('add', _.bind(this.render, this));
